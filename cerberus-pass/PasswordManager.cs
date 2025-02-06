e6f439b18cf381686309b5d4e63080686f6d65a7
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2025-02-06 15:59:40 +0100

    MVP for storing passwords in ram done

--- cerberus-pass/PasswordManager.cs
+++ cerberus-pass/PasswordManager.cs
@@ -2,5 +2,33 @@ namespace cerberus_pass;
 
 public class PasswordManager
 {
+  private List<PasswordEntry> vault;
 
+  public PasswordManager()
+  {
+    // vault = new List<PasswordEntry>();
+    // vault = new();
+    // vault = [];
+    vault = [];
+  }
+
+  public List<PasswordEntry> GetAll() => vault;
+
+  public PasswordEntry CreateEntry(
+    string title,
+    string login,
+    string password,
+    string website = "",
+    string note = "")
+  {
+    var newEntry = new PasswordEntry(
+      title,
+      login,
+      password,
+      website,
+      note
+    );
+    vault.Add(newEntry);
+    return newEntry;
+  }
 }
--- cerberus-pass/Program.cs
+++ cerberus-pass/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Main UI-Flow
+using System.Text.RegularExpressions;
 using cerberus_pass;
 
 var pass1 = new PasswordEntry("Steam", "WaldmeisterSD", "P@ssword");
@@ -13,16 +14,59 @@ var pass3 = new PasswordEntry(
   "Der geilste Service um alte Videospiele zu erwerben!"
 );
 
+var manager = new PasswordManager();
+
 Console.ForegroundColor = ConsoleColor.DarkRed;
 Console.WriteLine("Willkommen zu Cerberus-Pass!");
 Console.ResetColor();
 
-Console.WriteLine("Wähle was du tun willst:");
+do
+{
+  Console.WriteLine("Wähle was du tun willst:");
 
-Console.WriteLine("""
+  Console.WriteLine("""
   1. Passwort-Liste ausgeben
   2. Passwort mit ID ausgeben
   3. Neues Passwort erstellen
   4. Vorhandenes Passwort bearbeiten
   5. Passwort löschen
 """);
+
+  var userInput = Console.ReadLine();
+
+  switch (userInput)
+  {
+    case "1":
+      // Liste ausgeben
+      var vault = manager.GetAll();
+      Console.WriteLine(vault);
+      break;
+    case "2":
+      // Passwort anhand ID ausgeben
+      break;
+    case "3":
+      // Erstellen
+      Console.WriteLine("Gebe einen Titel für den Eintrag an:");
+      var title = Console.ReadLine();
+      Console.WriteLine("Gebe einen Login für den Eintrag an:");
+      var login = Console.ReadLine();
+      Console.WriteLine("Gebe ein Passwort für den Eintrag an:");
+      var password = Console.ReadLine();
+      var newEntry = manager.CreateEntry(title, login, password);
+      Console.WriteLine("Neuer Eintrag erfolgreich erstellt:");
+      Console.WriteLine(newEntry); // Gibt Type aus;
+      break;
+    case "4":
+      // Update
+      break;
+    case "5":
+      // Delete
+      break;
+    default:
+      // Fehler anzeigen -> Eingabe-Hint (1-5)
+      // Eingabe wiederholen
+      break;
+  }
+  Console.ReadKey();
+  Console.Clear();
+} while (true);
