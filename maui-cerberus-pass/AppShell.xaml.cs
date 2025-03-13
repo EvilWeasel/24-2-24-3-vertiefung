237b4ef70d121cfa6a22d14499078e3247184ddb
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-03-13 12:02:09 +0100

    Basic layout for new maui-password-manager gui app MainPage

---
+++ maui-cerberus-pass/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace maui_cerberus_pass
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ maui-cerberus-pass/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace maui_cerberus_pass;
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<PasswordEntry> Entries { get; set; } = [];
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        Entries.Add(new PasswordEntry(
+            "github-privat",
+            "evilweasel",
+            "P@ssword"
+        ));
+        Entries.Add(new PasswordEntry(
+            "github-arbeit",
+            "boilerplatesharp",
+            "P@ssword"
+        ));
+        Entries.Add(new PasswordEntry(
+            "steam",
+            "evilweasel",
+            "P@ssword"
+        ));
+        Entries.Add(new PasswordEntry(
+            "gog",
+            "waldmeistersd",
+            "P@ssword"
+        ));
+    }
+}
