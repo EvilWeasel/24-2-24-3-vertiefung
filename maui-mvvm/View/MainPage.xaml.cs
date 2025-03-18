6c1c6c97c429a02ce4335d0fc14120cf0f744dc9
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-03-18 14:01:18 +0100

    basic viewmodel with inotifypropertychanged

--- maui-mvvm/View/MainPage.xaml.cs
+++ maui-mvvm/View/MainPage.xaml.cs
@@ -1,24 +1,14 @@
 ﻿using maui_mvvm.Model;
+using maui_mvvm.ViewModel;
 
 namespace maui_mvvm.View;
 public partial class MainPage : ContentPage
 {
-    public List<Person> People = new List<Person>();
-    public Person MyPerson { get; set; } 
-       = new Person("Tobi", "Wehrle");
     public MainPage()
     {
         // MyPerson = new Person("Alexander", "Galagan");
         InitializeComponent(); // hier wird data-binding gesetzt
         // MyPerson = new Person("Alexander", "Galagan");
-        BindingContext = this;
-    }
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        People.Add(
-            new Person(MyPerson.FirstName, MyPerson.LastName));
-        MyPerson.FirstName = string.Empty;
-        MyPerson.LastName = string.Empty;
+        BindingContext = new MainViewModel();
     }
 }
--- maui-mvvm/ViewModel/MainViewModel.cs
+++ maui-mvvm/ViewModel/MainViewModel.cs
@@ -1,4 +1,32 @@
-﻿namespace maui_mvvm.ViewModel;
-class MainViewModel
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using maui_mvvm.Model;
+
+namespace maui_mvvm.ViewModel;
+public class MainViewModel : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnProptertyChanged([CallerMemberName] string? name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    public List<Person> People = new List<Person>();
+    public Person MyPerson { get; set; }
+       = new Person("Tobi", "Wehrle");
+    private string testViewModel = string.Empty;
+    public string TestViewModel
+    {
+        get => testViewModel;
+        set
+        {
+            testViewModel = value;
+            OnProptertyChanged();
+        }
+    }
+
+    public void Button_Clicked(object sender, EventArgs e)
+    {
+        People.Add(
+            new Person(MyPerson.FirstName, MyPerson.LastName));
+        MyPerson.FirstName = string.Empty;
+        MyPerson.LastName = string.Empty;
+    }
 }
