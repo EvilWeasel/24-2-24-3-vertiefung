559bc9759747f6e28a7727a513676dd9f31ed201
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-03-19 09:52:55 +0100

    changed usage of relay command to CommunityToolkit.Mvvm provided one in MainViewModel.cs

--- maui-mvvm/ViewModel/MainViewModel.cs
+++ maui-mvvm/ViewModel/MainViewModel.cs
@@ -2,14 +2,15 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using maui_mvvm.Helper;
 using maui_mvvm.Model;
 
 namespace maui_mvvm.ViewModel;
 public partial class MainViewModel : BaseViewModel
 {
-    public RelayCommand AddCommand => new RelayCommand(execute => AddPerson());
-    public RelayCommand ShowAlertCommand => new RelayCommand(execute => ShowAlert());
+    // public RelayCommand AddCommand => new RelayCommand(execute => AddPerson());
+    // public RelayCommand ShowAlertCommand => new RelayCommand(execute => ShowAlert());
 
     public ObservableCollection<Person> People = Person.People;
     public Person MyPerson { get; set; }
@@ -17,6 +18,7 @@ public partial class MainViewModel : BaseViewModel
     [ObservableProperty]
     private string testViewModel = string.Empty;
 
+    [RelayCommand]
     public void AddPerson()
     {
         People.Add(
@@ -25,6 +27,7 @@ public partial class MainViewModel : BaseViewModel
         MyPerson.LastName = string.Empty;
     }
 
+    [RelayCommand]
     public void ShowAlert()
     {
         Shell.Current.DisplayAlert("ALARM", "Nice Job!", "Weiter im Text");
