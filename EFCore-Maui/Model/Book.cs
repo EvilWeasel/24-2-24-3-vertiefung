20ed319c3db1e81258f41f317ea0e5aecb4ad466
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-04-04 13:21:03 +0200

    added maui efcore docs and demo

---
+++ EFCore-Maui/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EFCore_Maui.Model;
+public class Book
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Author { get; set; }
+    [Required]
+    [MinLength(10)]
+    [MaxLength(14)]
+    public string ISBN { get; set; }
+    public string Category { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+    public string ImageSourceUrl { get; set; }
+
+}
---
+++ EFCore-Maui/README.md
@@ -0,0 +1,8 @@
+# EFCore und Dotnet MAUI
+
+Weil Dotnet MAUI unter IOS und Android einige essentiellen Teile von EFCore aus der kompilierten App "strippen", müssen wir entweder, wenn wir für diese Plattformen entwickeln wollen, einen Work-Around nutzen...
+Oder wir entfernen diese Target-Frameworks einfach aus der Projektmappe:
+
+```csharp
+
+```
